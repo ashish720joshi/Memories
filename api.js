8e22b0cbeb5ea2f19f27384838670917ff2fcc61
Ashish Joshi <AJoshi28@slb.com>
2021-02-16 21:03:43 +0530

    added new apis and routing

--- api.js
+++ api.js
@@ -1,9 +1,29 @@
 var activity=require('./Activity');
 var express=require('express');
+var app=express();
 const router=express.Router();
-router.get('/',dbOperations.getPosts);
+
 var dbOperations=require('./dbOperation');
-dbOperations.getPosts().then(result=>{
-    console.log("result"+result);
-})
+app.use(express.json());
+
+const port =process.env.PORT || 5000;
+
+app.get('/getPosts',dbOperations.getPosts);
+app.get('/getPost/:id',dbOperations.getPostsById);
+app.get('/getActivities',dbOperations.getAcitivites);
+app.get('/getComments',dbOperations.getComments);
+app.get('/getLikes/:id',dbOperations.getLikes);
+app.get('/signIn',dbOperations.signIn);
+app.post('/addPosts',dbOperations.addPosts);
+app.post('/addComment',dbOperations.addComments);
+app.post('/addActivity',dbOperations.addActivity);
+app.post('/addUsers',dbOperations.addUsers);
+app.put('/like',dbOperations.like);
+
+app.listen(port, function(){
+    console.log('Listening on port ' + port); //Listening on port 8888
+});
+// dbOperations.getPosts().then(result=>{
+//     console.log("result"+result);
+// })
 
--- dbOperation.js
+++ dbOperation.js
@@ -2,7 +2,7 @@ var config = require('./dbconfig');
 const sql = require('mssql/msnodesqlv8');
 const moment = require('moment');
 
-getAcitivites = async () => {
+getAcitivites = async (request,response) => {
 
     try {
         let conn = new sql.ConnectionPool(config);
@@ -11,10 +11,10 @@ getAcitivites = async () => {
         let connect = await conn.connect();
         let activites = await req.query('select * from Activity');
         console.log(activites);
-
-        // let activites=await pool.request().query('select * from Activity');
-        // return activites.recordsets;
+        
+        response.send(activites);
         conn.close();
+       
     }
     catch (e) {
         console.log(e);
@@ -23,7 +23,7 @@ getAcitivites = async () => {
 
 
 //check date formate later
-addUsers = async () => {
+addUsers = async (request,res) => {
     //     let currentDate=new Date().toISOString();
     //     console.log(currentDate);
     //      currentDate=currentDate.split('.')[0];
@@ -37,13 +37,15 @@ addUsers = async () => {
         let req = new sql.Request(conn);
 
         let connect = await conn.connect();
-        let query = `INSERT INTO [dbo].[Users] ([Name], [Email], [metaCreatedAt] ) VALUES('Ahsaas Nathawat','an@gmail.com',CONVERT(date,GETDATE()))`;
-
+        let query = `INSERT INTO [dbo].[Users] ([Name], [Email], [metaCreatedAt] ) VALUES('Ahsaas Nathawat','an@gmail.com',CONVERT(date,GETDATE()));SELECT SCOPE_IDENTITY();`;
+       
         let response = await req.query(query);
-        console.log(response);
+        let userId = Object.values(response.recordset[0])[0];
+        console.log(userId);
+        let responseBody={name:request.params.name,
+        id:userId}
+        res.send(responseBody);
 
-        // let activites=await pool.request().query('select * from Activity');
-        // return activites.recordsets;
         conn.close();
     }
     catch (e) {
@@ -51,7 +53,7 @@ addUsers = async () => {
     }
 }
 
-addActivity = async () => {
+addActivity = async (request,res) => {
 
 
     let currentDate = new Date().toISOString();
@@ -59,7 +61,7 @@ addActivity = async () => {
     currentDate = currentDate.split('.')[0];
     currentDate = currentDate.replace('T', ' ');
     console.log(currentDate);
-    let query = `INSERT INTO [dbo].[Activity] ([type_], [likes], [metaCreatedAt] ) VALUES('Post',${0},CONVERT(date,GETDATE()))`;
+    let query = `INSERT INTO [dbo].[Activity] ([type_], [likes], [metaCreatedAt] ) VALUES('Post',${0},CONVERT(date,GETDATE()));SELECT SCOPE_IDENTITY();`;
     try {
         let conn = new sql.ConnectionPool(config);
         let req = new sql.Request(conn);
@@ -67,6 +69,12 @@ addActivity = async () => {
         let connect = await conn.connect();
         let response = await req.query(query);
         console.log(response);
+        let activityId = Object.values(response.recordset[0])[0];
+        console.log(activityId);
+        let responseBody={name:request.params.name,
+            id:activityId}
+            res.send(responseBody);
+
 
         // let activites=await pool.request().query('select * from Activity');
         // return activites.recordsets;
@@ -77,7 +85,7 @@ addActivity = async () => {
     }
 }
 
-addPosts = async () => {
+addPosts = async (request,res) => {
     let currentDate = new Date().toISOString();
     console.log(currentDate);
     currentDate = currentDate.split('.')[0];
@@ -92,9 +100,15 @@ addPosts = async () => {
         let connect = await conn.connect();
         let response = await req.query(query1);
         let activityId = Object.values(response.recordset[0])[0];
-        let query = `INSERT INTO [dbo].[Posts] ([postName],  [metaCreatedAt],[userId],[activityId] ) VALUES('first',CONVERT(date,GETDATE()),'1',${activityId})`;
+        let query = `INSERT INTO [dbo].[Posts] ([postName],  [metaCreatedAt],[userId],[activityId] ) VALUES('first',CONVERT(date,GETDATE()),'1',${activityId});SELECT SCOPE_IDENTITY();`;
         let response2 = await req.query(query);
         console.log(response);
+        let postId = Object.values(response.recordset[0])[0];
+        console.log(postId);
+        let responseBody={name:request.params.name,
+            id:postId}
+            res.send(responseBody);
+        
 
         // let activites=await pool.request().query('select * from Activity');
         // return activites.recordsets;
@@ -105,7 +119,7 @@ addPosts = async () => {
     }
 }
 
-getPosts = async () => {
+getPosts = async (request,res) => {
 
     try {
         let conn = new sql.ConnectionPool(config);
@@ -114,7 +128,7 @@ getPosts = async () => {
         let connect = await conn.connect();
         let posts = await req.query('select * from Posts order by metaCreatedAt desc');
         console.log(posts);
-
+        res.send(posts);
         // let activites=await pool.request().query('select * from Activity');
         // return activites.recordsets;
         conn.close();
@@ -124,11 +138,30 @@ getPosts = async () => {
     }
 }
 
+getPostsById=async(request,response)=>{
+    try{
+        let conn = new sql.ConnectionPool(config);
+        let req = new sql.Request(conn);
+
+        let connect = await conn.connect();
+        let post = await req.query(`select * from Posts where activityId=${request.params.id}`);
+        console.log(post);
+
+        response.send(post);
+        conn.close();
+       
+
+    }catch(e)
+    {
+        console.log(e);
+    }
+}
 
 
 
 
-addComments = async () => {
+
+addComments = async (request,res) => {
     let currentDate = new Date().toISOString();
     console.log(currentDate);
     currentDate = currentDate.split('.')[0];
@@ -146,10 +179,15 @@ addComments = async () => {
         let activityId = Object.values(response.recordset[0])[0];
         console.log(activityId);
 
-        let query = `INSERT INTO [dbo].[Comments] ([comment],  [metaCreatedAt],[userId],[activityId] ) VALUES('first',CONVERT(date,GETDATE()),'1',${activityId})`;
+        let query = `INSERT INTO [dbo].[Comments] ([comment],  [metaCreatedAt],[userId],[activityId] ) VALUES('first',CONVERT(date,GETDATE()),'1',${activityId});SELECT SCOPE_IDENTITY();`;
 
         let response2 = await req.query(query);
         console.log(response2)
+        let commentId = Object.values(response2.recordset[0])[0];
+        console.log(commentId);
+        let responseBody={name:request.params.name,
+            id:commentId}
+            res.send(responseBody);
 
         // let activites=await pool.request().query('select * from Activity');
         // return activites.recordsets;
@@ -160,15 +198,15 @@ addComments = async () => {
     }
 }
 
-likeAPost = async () => {
+like = async (request,res) => {
     let currentDate = new Date().toISOString();
     console.log(currentDate);
     currentDate = currentDate.split('.')[0];
     currentDate = currentDate.replace('T', ' ');
     console.log(currentDate);
-    let activityId=2;
-    let userId=1;
-    let likes=0;
+    let activityId=request.params.activityId;
+    let userId=request.params.userId;
+    let likes=request.params.likes;
     let query = `INSERT INTO [dbo].[Likes]
     ([type],[metaCreatedAt],[userId],[activityId])VALUES('Comment',CONVERT(date,GETDATE()),${userId},${activityId})`;
 
@@ -184,7 +222,7 @@ likeAPost = async () => {
         let response2=await req.query(query1);
         console.log(response);
         console.log(response2);
-
+        res.sendStatus(200);
         // let activites=await pool.request().query('select * from Activity');
         // return activites.recordsets;
         conn.close();
@@ -194,7 +232,7 @@ likeAPost = async () => {
     }
 }
 
-getComments = async () => {
+getComments = async (request,res) => {
 
     try {
         let conn = new sql.ConnectionPool(config);
@@ -202,8 +240,9 @@ getComments = async () => {
         let activityId=1;
 
         let connect = await conn.connect();
-        let posts = await req.query(`select * from [dbo].[Comments] where [activityId]=${activityId} order by metaCreatedAt desc`);
+        let comments = await req.query(`select * from [dbo].[Comments] where [activityId]=${activityId} order by metaCreatedAt desc`);
         console.log(posts);
+        res.send(comments);
 
         // let activites=await pool.request().query('select * from Activity');
         // return activites.recordsets;
@@ -214,16 +253,17 @@ getComments = async () => {
     }
 }
 
-getLikes = async () => {
+getLikes = async (request,res) => {
 
     try {
         let conn = new sql.ConnectionPool(config);
         let req = new sql.Request(conn);
-        let activityId=1;
+        let activityId=request.params.id;
 
         let connect = await conn.connect();
-        let posts = await req.query(`select * from [dbo].[Likes] where [activityId]=${activityId} order by metaCreatedAt desc`);
-        console.log(posts);
+        let likesCount = await req.query(`select likes from [dbo].[Activity] where [activityId]=${activityId} order by metaCreatedAt desc`);
+        console.log(likesCount);
+        res.send(likesCount.recordset[0]);
 
         // let activites=await pool.request().query('select * from Activity');
         // return activites.recordsets;
@@ -235,7 +275,7 @@ getLikes = async () => {
 }
 
 
-signIn = async () => {
+signIn = async (request,res) => {
 
     try {
         let conn = new sql.ConnectionPool(config);
@@ -256,6 +296,104 @@ signIn = async () => {
     }
 }
 
+deleteAPost=async(request,res)=>{
+    try {
+        let conn = new sql.ConnectionPool(config);
+        let req = new sql.Request(conn);
+        let postId=request.params.postId;
+        let activityId=request.params.activityId;
+
+        let connect = await conn.connect();
+        let response = await req.query(`delete [dbo].[Posts]  where postId=${postId}; delete [dbo].[Activity] where [activityId]=${activityId} `);
+        console.log(response);
+       res.sendStatus(200);
+
+        // let activites=await pool.request().query('select * from Activity');
+        // return activites.recordsets;
+        conn.close();
+    }
+    catch (e) {
+        console.log(e);
+    }
+}
+
+deleteAComment=async(request,res)=>{
+    try {
+        let conn = new sql.ConnectionPool(config);
+        let req = new sql.Request(conn);
+        let commentId=request.params.commentId;
+        let activityId=request.params.activityId;
+
+        let connect = await conn.connect();
+        let response = await req.query(`delete [dbo].[Comments]  where postId=${commentId}; delete [dbo].[Activity] where [activityId]=${activityId} `);
+        console.log(response);
+       res.sendStatus(200);
+
+        // let activites=await pool.request().query('select * from Activity');
+        // return activites.recordsets;
+        conn.close();
+    }
+    catch (e) {
+        console.log(e);
+    }
+}
+
+deleteAUser=async(request,res)=>{
+    try {
+        let conn = new sql.ConnectionPool(config);
+        let req = new sql.Request(conn);
+        let userId=request.params.id;
+      
+
+        let connect = await conn.connect();
+        let response = await req.query(`delete [dbo].[Users]  where [userId]=${userId}`);
+        console.log(response);
+       res.sendStatus(200);
+
+        // let activites=await pool.request().query('select * from Activity');
+        // return activites.recordsets;
+        conn.close();
+    }
+    catch (e) {
+        console.log(e);
+    }
+}
+
+
+
+unlike = async (request,res) => {
+    let currentDate = new Date().toISOString();
+    console.log(currentDate);
+    currentDate = currentDate.split('.')[0];
+    currentDate = currentDate.replace('T', ' ');
+    console.log(currentDate);
+    let activityId=request.params.activityId;
+    let userId=request.params.userId;
+    let likes=request.params.likes;
+    let query = `delete [dbo].[Likes]
+   where userId=${userId}`;
+
+    let query1=`Update [dbo].[Activity]
+    set likes=${likes-1}
+    where activityId=${activityId};`;
+    try {
+        let conn = new sql.ConnectionPool(config);
+        let req = new sql.Request(conn);
+
+        let connect = await conn.connect();
+        let response = await req.query(query);
+        let response2=await req.query(query1);
+        console.log(response);
+        console.log(response2);
+        res.sendStatus(200);
+        // let activites=await pool.request().query('select * from Activity');
+        // return activites.recordsets;
+        conn.close();
+    }
+    catch (e) {
+        console.log(e);
+    }
+}
 
 
 
@@ -270,8 +408,13 @@ module.exports = {
     addPosts: addPosts,
     getPosts: getPosts,
     addComments: addComments,
-    likeAPost:likeAPost,
+    like:like,
     getComments:getComments,
     getLikes:getLikes,
-    signIn:signIn
+    signIn:signIn,
+    getPostsById:getPostsById,
+    deleteAPost:deleteAPost,
+    deleteAComment:deleteAComment,
+    deleteAUser:deleteAUser,
+    unlike:unlike
 }
